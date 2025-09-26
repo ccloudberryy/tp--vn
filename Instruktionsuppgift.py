d404948390d6d9e8771ed55280f9dc1875d10d02
ccloudberryy <elviraaany@gmail.com>
2025-09-26 13:24:43 +0200

    Instruktionsuppgift med battle

--- Instruktionsuppgift.py
+++ Instruktionsuppgift.py
@@ -5,7 +5,6 @@ import random
 name = input("Vad heter du? ")
 life_lost = 4
 demon = 20
-
 while True:
     if name.lower() == "jens":
         choice = input("Nu, Jens, är det dags för att lära ut eltreorna! Du får totalt 4 liv. Hälsar du på dina elever? [Y]es/[N]o: ")
@@ -21,7 +20,7 @@ while True:
             if choice.lower() == "y" or choice.lower() == "yes":
                 print("Alla sätter bort sina telefoner, men de lyssnar inte. -1 liv")
                 life_lost -= 1
-        
+            
             if choice.lower() == "n" or choice.lower() == "no":
                 print("Alla lyssnar på dig när du säger till dem snällt. Du mår bra.")
         sleep(1)
@@ -30,7 +29,6 @@ while True:
         choice = input("Du förbereder presentationen, men du märker att Viggo håller på att göra ett pappersflygplan. Tar du papperet och kastar den i soptunnan? [Y]es/[N]o: ")
         if choice.lower() == "y" or choice.lower() == "yes":
             print("Alla skrattar åt Viggo! Du har fått klassens respekt. Du känner dig maktfull.")
-
         if choice.lower() == "n" or choice.lower() == "no":
             print("Viggo kastar pappersflygplanet på Gustav! Han blir ledsen och går ut ur klassrummet. -1 liv")
             life_lost -= 1
@@ -40,7 +38,6 @@ while True:
         choice = input("Det är 30 minuter kvar av lektionen. Alla jobbar intensivt. Lyssnar du på dina tankar och skriker? [Y]es/[N]o: ")
         if choice.lower() == "y" or choice.lower() == "yes":
             print("Alla stirrar på dig. Det tar en sekund, men Mohammad börjar garvskratta och sedan följer resten. Du känner dig rolig.")
-
         if choice.lower() == "n" or choice.lower() == "no":
             print("Ingenting händer, men du känner dig töntig som inte skrek. -1 liv")
             life_lost -= 1
@@ -72,7 +69,7 @@ while True:
         elif life_lost == 0:
             print("Liv kvar: 0")
             print("Hur lyckades du?")
-        break
+            break
 
         sleep(1)
         print("...")
